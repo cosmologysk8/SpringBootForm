4823f24d0660fd4ed71bce89df1df08824970bbe
cosmologysk8 <a1999afg@gmail.com>
2022-10-13 20:36:36 +0200

    he avanzado a la validacion de forms mucho mas xd

--- src/main/java/com/example/springbootform/controllers/FormController.java
+++ src/main/java/com/example/springbootform/controllers/FormController.java
@@ -1,26 +1,40 @@
 package com.example.springbootform.controllers;
 
-import com.example.springbootform.controllers.model.Usuario;
+import com.example.springbootform.model.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class FormController {
 
     @GetMapping("/form")
     public String form(Model model){
-
+        Usuario usuario = new Usuario();
+        model.addAttribute("usuario", usuario);
         model.addAttribute("titulo","Formulario de Usuario");
         return "form";
     }
 
     @PostMapping("/form")
-    public String procesar(Usuario usuario, Model model){
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model){
 
-
+        if(result.hasErrors()){
+//            Map<String, String> errores = new HashMap<>();
+//            result.getFieldErrors().forEach(err ->{
+//                errores.put(err.getField(), "El campo :".concat(
+//                        err.getField().concat(" ")
+//                                .concat(err.getDefaultMessage())));
+//            });
+//            model.addAttribute("error", errores);
+            return "form";
+        }
         model.addAttribute("usuario", usuario);
         return "resultado";
     }
--- src/main/java/com/example/springbootform/model/Usuario.java
+++ src/main/java/com/example/springbootform/model/Usuario.java
@@ -1,16 +1,21 @@
-package com.example.springbootform.controllers.model;
+package com.example.springbootform.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
 public class Usuario {
 
+    @NotEmpty
     private String username;
+    @NotEmpty
     private String password;
+    @NotEmpty
     private String email;
 
 }
